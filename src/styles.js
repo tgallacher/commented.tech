33f088448e9275b50072712f8d2e469256c7bdaf
Tom Gallacher <tgallacher@users.noreply.github.com>
2019-09-11 22:27:18 +0100

    add styles for code highlighting

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,5 @@
 import { css } from '@emotion/core';
-import { lighten } from 'polished';
+import { lighten, darken } from 'polished';
 /**
  * Palette colors
  *
@@ -86,10 +86,22 @@ export const gatsbyHighlight = css`
   overflow: auto;
   border-radius: 0.3em;
   border: 1px solid ${PRIMARY_COLOR_DARK};
+  ${'' /* padding: 1em; */}
 
+  /**
+   * Remove the default PrismJS theme background-color, border-radius, margin,
+   * padding and overflow.
+   * 1. Make the element just wide enough to fit its content.
+   * 2. Always fill the visible space in .gatsby-highlight.
+   * 3. Adjust the position of the line numbers
+   */
   pre[class*='language-'] {
     padding: 0;
-    margin: 0;
+    margin: 1em 0;
+    overflow: initial;
+    float: left; /* 1 */
+    min-width: 100%; /* 2 */
+    background-color: ${darken(0.02, PRIMARY_COLOR_DARK)};
 
     &.line-numbers {
       padding: 0.5em 0 0.5em 2.8em;
@@ -100,5 +112,15 @@ export const gatsbyHighlight = css`
       top: 0.5em;
       bottom: 0.5em;
     }
+  }
+
+  .gatsby-highlight-code-line {
+    background-color: ${lighten(0.05, PRIMARY_COLOR_DARK)};
+    display: block;
+    margin-right: -1em;
+    margin-left: -1em;
+    padding-right: 1em;
+    padding-left: 0.75em;
+    border-left: 0.25em solid ${lighten(0.0, ACCENT_COLOR)};
   }
 `;
