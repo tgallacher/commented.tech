6b50f3739cfadd866e5157d007183037632c319d
Tom Gallacher <tgallacher@users.noreply.github.com>
2019-09-28 13:09:22 +0100

    feat: wip tmp remove Prism component for code blocks

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -3,8 +3,7 @@ import React from 'react';
 import Prism from '@theme-ui/prism';
 
 export default {
-  // ...Styled,
-  pre: props => props.children,
-  // pre: ({ children }) => <>{children}</>,
-  code: props => <Prism {...props} />,
+  ...Styled,
+  // pre: props => props.children,
+  // code: Prism,
 };
