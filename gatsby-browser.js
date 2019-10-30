af75d010313945a24d01725336737502a92a6759
Tom Gallacher <tgallacher@users.noreply.github.com>
2019-10-30 22:12:45 +0000

    styles: re-add browser normalisation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-// import 'normalize.css';
+import 'normalize.css';
 import 'prismjs/themes/prism-okaidia.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import '@fortawesome/fontawesome-free/css/all.css';
